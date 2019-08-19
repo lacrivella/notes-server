6a5a46a39f91649c2e2cc07787d4d1f8f0e1d6ff
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-08-19 11:44:23 -0700

    created get all route

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Note = require('../lib/models/Note');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -29,6 +30,22 @@ describe('app routes', () => {
           body: 'Task',
           __v: 0
         });
+      });
+  });
+
+  it('can get all notes', async() => {
+    const notes = await Note.create([
+      { title: 'one', body: 'task a' },
+      { title: 'two', body: 'task b' },
+      { title: 'three', body: 'task c' },
+    ]);
+    return request(app)
+      .get('/api/v1/notes')
+      .then(res => {
+        const noteJSON = JSON.parse(JSON.stringify(notes));
+        noteJSON.forEach((note => {
+          expect(res.body).toContainEqual({ title: note.title, body: note.body, _id: note._id, __v: 0 });
+        }));
       });
   });
 });
